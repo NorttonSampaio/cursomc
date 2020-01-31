8719ae8b68fce96fcb89e695a6700e381f51d581
Nortton Sampaio <norttonsampaio@gmail.com>
2020-01-31 10:06:59 -0300

    Adicionado entity estado e cidade

--- src/main/java/com/sampaio/cursomc/CursomcApplication.java
+++ src/main/java/com/sampaio/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sampaio.cursomc.domain.Categoria;
+import com.sampaio.cursomc.domain.Cidade;
+import com.sampaio.cursomc.domain.Estado;
 import com.sampaio.cursomc.domain.Produto;
 import com.sampaio.cursomc.repositories.CategoriaRepository;
+import com.sampaio.cursomc.repositories.CidadeRepository;
+import com.sampaio.cursomc.repositories.EstadoRepository;
 import com.sampaio.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -39,6 +47,19 @@ public class CursomcApplication implements CommandLineRunner{
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		Estado est1 = new Estado("Minas Gerais");
+		Estado est2 = new Estado("São Paulo");
+		
+		Cidade c1 = new Cidade("Uberlândia", est1);
+		Cidade c2 = new Cidade("São Paulo", est2);
+		Cidade c3 = new Cidade("Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
